c66b6ed9f45b9280f5b865bacb247b4a7f032ba0
indatim <indatim@gmail.com>
2023-03-12 15:12:36 +0000

    Deploying to gh-pages from @ indatim/goit-js-hw-09@604dfc602097e8d8620566475dbc8f40f09f4987 🚀

---
+++ 01-color-switcher.573b1bed.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,t.stopBtn.disabled=!1,timerId=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.stopBtn.addEventListener("click",(function(){clearInterval(timerId),t.startBtn.disabled=!1,t.stopBtn.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.573b1bed.js.map
